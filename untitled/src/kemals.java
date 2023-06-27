d85c50ebb1e77e4edfabfde7695631e5c89907c7
Realdealkemal <kemal_burak_fb@hotmail.com>
2023-06-27 19:07:21 +0300

    3.ncü sınıf ekledim

--- untitled/src/kemals.java
+++ untitled/src/kemals.java
@@ -1,4 +1,4 @@
-public class kemal {
+public class kemals {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
